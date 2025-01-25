d5aaf0ddfa7b01ce04eb280329a73e0002160556
Samuli <samuli.jaaskelainen@varjo.com>
2025-01-25 21:27:43 +0200

    Optimize level spawning.

--- GGJ2025/Assets/Scripts/LevelGenerator.cs
+++ GGJ2025/Assets/Scripts/LevelGenerator.cs
@@ -16,13 +16,21 @@ public class Biome
 
 public class LevelGenerator : MonoBehaviour
 {
+    public Transform player;
     public List<Biome> biomes = new List<Biome>();
     public int currentBiome = 0;
+    public int spawnCount = 100;
+    public int extraPiecesCount = 5;
+
     float nextPieceSpawnY = 0.0f;
+    List<GameObject> spawnedPieces = new List<GameObject>();
+    List<GameObject> extraPieces = new List<GameObject>();
+    bool spawnMorePieces = false;
+
 
     void Start()
     {
-        for(int i = 0; i < 100; ++i)
+        for(int i = 0; i < spawnCount; ++i)
         {
             SpawnNextPiece();
         }
@@ -30,13 +38,40 @@ public class LevelGenerator : MonoBehaviour
 
     void Update()
     {
-        
+        if (player.position.y < spawnedPieces[spawnedPieces.Count - 1].transform.position.y)
+        {
+            if(extraPieces.Count > 0)
+            {
+                for (int i = 0; i < extraPieces.Count; ++i)
+                {
+                    Destroy(extraPieces[i]);
+                }
+            }
+            extraPieces.Clear();
+
+            for (int i = 0; i < spawnCount - extraPiecesCount; ++i)
+            {
+                Destroy(spawnedPieces[i]);
+            }
+
+            for (int i = spawnCount - extraPiecesCount; i < spawnCount; ++i)
+            {
+                extraPieces.Add(spawnedPieces[i]);
+            }
+
+            spawnedPieces.Clear();
+
+            for (int i = 0; i < spawnCount; ++i)
+            {
+                SpawnNextPiece();
+            }
+        }
     }
 
     void SpawnNextPiece()
     {
         LevelPiece nextPiece = biomes[currentBiome].pieces[Random.Range(0, biomes[currentBiome].pieces.Length)];
-        Instantiate(nextPiece.prefab, new Vector3(0.0f, nextPieceSpawnY, 0.0f), Quaternion.identity);
+        spawnedPieces.Add(Instantiate(nextPiece.prefab, new Vector3(0.0f, nextPieceSpawnY, 0.0f), Quaternion.identity));
         nextPieceSpawnY -= nextPiece.height;
     }
 }
