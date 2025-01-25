8338130cb997196d5f2537c91aa6eab19a4f155d
Samuli <samuli.jaaskelainen@varjo.com>
2025-01-25 17:13:14 +0200

    Add fish, hurting and camera prediction.

---
+++ GGJ2025/Assets/Scripts/Fish.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : MonoBehaviour
+{
+    public Transform path;
+    public float speed = 1.0f;
+    public float wave = 1.0f;
+
+    List<Vector3> pathNodes = new List<Vector3>();
+    int targetNode = 0;
+    float random;
+
+    void Start()
+    {
+        for(int i = 0; i < path.childCount; ++i)
+        {
+            pathNodes.Add(path.GetChild(i).transform.position);
+            if(i > 0)
+            { 
+                Debug.DrawLine(pathNodes[i - 1], pathNodes[i], Color.red, 30.0f);
+            }
+        }
+        random = Random.value;
+    }
+
+    void Update()
+    {
+        Vector3 target = pathNodes[targetNode] + (Vector3.up * wave * Mathf.Sin(Time.time + random));
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        
+        Quaternion targetRot = Quaternion.LookRotation(target - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, target) < 0.3f)
+        {
+            targetNode++;
+            if(targetNode >= pathNodes.Count)
+            {
+                targetNode = 0;
+            }
+        }
+    }
+}
--- GGJ2025/Assets/Scripts/Player.cs
+++ GGJ2025/Assets/Scripts/Player.cs
@@ -3,6 +3,11 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    public Transform sceneCamera;
+    public float cameraDistance = 10.0f;
+    public float maxCameraPredictionMultiplier = 1.0f;
+    public float maxCameraPrediction = 1.0f;
+    public float cameraSmoothness = 10.0f;
     public float currentScore;
     public bool isDead;
     public float oxygen = 100.0f;
@@ -12,7 +17,13 @@ public class Player : MonoBehaviour
     public float dashTime = 0.33f;
     public float dashForce = 4000.0f;
     public float dashMaxSpeed = 9.0f;
+    public float hurtTime = 0.5f;
+    public float hurtForce = 1.0f;
     public GameObject[] oxygenLights;
+    public Light headLight;
+    public Color warningHeadlightColor;
+    public AnimationCurve warningHeadlightAnim;
+    public float warningHeadlightSpeed;
 
     InputAction moveAction;
     InputAction dashAction;
@@ -20,12 +31,20 @@ public class Player : MonoBehaviour
     Rigidbody rb;
     float dashTimer;
     bool isDashing;
+    float hurtTimer;
+    bool isHurt;
+    Vector3 hurtDir;
+    Color defaultHeadlightColor;
+    float defaultHeadlightIntensity;
+    float warningAnimTime;
 
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
         dashAction = InputSystem.actions.FindAction("Dash");
         rb = GetComponent<Rigidbody>();
+        defaultHeadlightColor = headLight.color;
+        defaultHeadlightIntensity = headLight.intensity;
     }
 
     void Update()
@@ -39,6 +58,23 @@ public class Player : MonoBehaviour
             dashTimer = Time.time + dashTime;
         }
         isDashing = dashTimer > Time.time;
+
+        isHurt = hurtTimer > Time.time;
+        if(isHurt)
+        {
+            headLight.color = warningHeadlightColor;
+            headLight.intensity = defaultHeadlightIntensity * warningHeadlightAnim.Evaluate(warningAnimTime);
+            warningAnimTime += warningHeadlightSpeed * Time.deltaTime;
+            if(warningAnimTime > 1.0f)
+            {
+                warningAnimTime -= 1.0f;
+            }
+        }
+        else
+        {
+            headLight.color = defaultHeadlightColor;
+            headLight.intensity = defaultHeadlightIntensity;
+        }
 
         moveDir = moveAction.ReadValue<Vector2>() * Time.deltaTime * (isDashing ? dashForce : force);
         oxygen -= moveAction.ReadValue<Vector2>().magnitude * Time.deltaTime;
@@ -66,6 +102,11 @@ public class Player : MonoBehaviour
         if (isDead)
             return;
 
+        if (isHurt)
+        {
+            moveDir = hurtDir * hurtForce;
+        }
+
         Vector3 addForce = new Vector3(moveDir.x, moveDir.y, 0.0f);
         rb.AddForce(addForce, ForceMode.Acceleration);
 
@@ -73,6 +114,25 @@ public class Player : MonoBehaviour
         if (rb.linearVelocity.magnitude > currentMaxSpeed)
         {
             rb.linearVelocity = rb.linearVelocity.normalized * currentMaxSpeed;
+        }
+    }
+
+    void LateUpdate()
+    {
+        float y = rb.linearVelocity.y * maxCameraPredictionMultiplier;
+        y = Mathf.Clamp(y, -maxCameraPrediction, 0.0f);
+        y += transform.position.y;
+        Vector3 target = new Vector3(0.0f, y, transform.position.z - cameraDistance);
+        sceneCamera.transform.position = Vector3.Lerp(sceneCamera.transform.position, target, cameraSmoothness * Time.smoothDeltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == "Hurt")
+        {
+            hurtTimer = Time.time + hurtTime;
+            hurtDir = (collision.GetContact(0).point - collision.transform.position).normalized;
+            Debug.DrawLine(collision.GetContact(0).point, collision.GetContact(0).point + hurtDir, Color.white, 10.0f);
         }
     }
 }
